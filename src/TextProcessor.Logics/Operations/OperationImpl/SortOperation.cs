87419954fc7fb8345d572121361b00207a828971
Funny_Silkie <ban.grumble99rar@gmail.com>
2023-09-16 06:42:00 +0900

    update sort operation

--- src/TextProcessor.Logics/Operations/OperationImpl/SortOperation.cs
+++ src/TextProcessor.Logics/Operations/OperationImpl/SortOperation.cs
@@ -10,34 +10,23 @@ namespace TextProcessor.Logics.Operations.OperationImpl
     [Serializable]
     internal class SortOperation : Operation
     {
-        /// <summary>
-        /// キーの列インデックスを取得または設定します。
-        /// </summary>
-        public int KeyIndex { get; set; }
-
-        /// <summary>
-        /// 数値として比較を行うかどうかを表す値を取得または設定します。
-        /// </summary>
-        public bool AsNumber { get; set; }
-
-        /// <summary>
-        /// 逆順ソートを行うかどうかを表す値を取得または設定します。
-        /// </summary>
-        public bool AsReversed { get; set; }
-
-        /// <summary>
-        /// 大文字小文字を区別するかどうかを表す値を取得または設定します。
-        /// </summary>
-        public bool CaseSensitive { get; set; } = true;
-
         /// <inheritdoc/>
         public override string Title => "並び替え";
+
+        /// <summary>
+        /// ソート条件一覧を取得または設定します。
+        /// </summary>
+        public List<SortEntry> Entries { get; set; }
 
         /// <summary>
         /// <see cref="SortOperation"/>の新しいインスタンスを初期化します。
         /// </summary>
         public SortOperation()
         {
+            Entries = new List<SortEntry>()
+            {
+                new SortEntry(),
+            };
         }
 
         /// <summary>
@@ -49,10 +38,7 @@ namespace TextProcessor.Logics.Operations.OperationImpl
         protected SortOperation(SortOperation cloned)
             : base(cloned)
         {
-            KeyIndex = cloned.KeyIndex;
-            CaseSensitive = cloned.CaseSensitive;
-            AsNumber = cloned.AsNumber;
-            AsReversed = cloned.AsReversed;
+            Entries = cloned.Entries.CloneAll();
         }
 
         /// <inheritdoc/>
@@ -64,27 +50,37 @@ namespace TextProcessor.Logics.Operations.OperationImpl
         /// <inheritdoc/>
         protected override IList<ArgumentInfo> GenerateArguments()
         {
+            var arg0 = new ArgumentInfo(ArgumentType.Arguments | ArgumentType.List, "ソート条件", () => Entries, x => Entries = x);
+            arg0.SetCtor(() => new SortEntry());
             return new[]
             {
-                new ArgumentInfo(ArgumentType.Index, "キーの列番号", () => KeyIndex, x => KeyIndex = x),
-                new ArgumentInfo(ArgumentType.Boolean, "逆順ソート", () => AsReversed, x => AsReversed = x),
-                new ArgumentInfo(ArgumentType.Boolean, "数値として比較", () => AsNumber, x => AsNumber = x),
-                new ArgumentInfo(ArgumentType.Boolean, "大文字小文字の区別", () => CaseSensitive, x => CaseSensitive = x),
+                arg0,
             };
         }
 
         /// <inheritdoc/>
         protected override void VerifyArgumentsCore(ProcessStatus status)
         {
+            if (Entries.Count == 0) status.Errors.Add(new StatusEntry(Title, Arguments[0], "ソート条件が記述されていません"));
         }
 
         /// <inheritdoc/>
         protected override void OperateCore(TextData data, ProcessStatus status)
         {
-            IComparer<List<string>> comparer = AsNumber ? new IndexedNumberComparer(KeyIndex) : new IndexedStringComparer(!CaseSensitive, KeyIndex);
+            if (Entries.Count == 0) return;
+
+            List<IComparer<List<string>>> comparers = Entries.ConvertAll(x => x.GetComparer());
             List<List<string>> list = data.GetSourceData();
             int offset = data.HasHeader ? 1 : 0;
-            Comparison<List<string>> comparison = AsReversed ? (x, y) => comparer.Compare(y, x) : comparer.Compare;
+            Comparison<List<string>> comparison = (x, y) =>
+            {
+                foreach (IComparer<List<string>> current in comparers)
+                {
+                    int result = current.Compare(x, y);
+                    if (result != 0) return result;
+                }
+                return 0;
+            };
             list.Sort(offset, list.Count - offset, Comparer<List<string>>.Create(comparison));
         }
 
@@ -148,6 +144,95 @@ namespace TextProcessor.Logics.Operations.OperationImpl
                 if (!isXNumber) dx = double.NaN;
                 if (!isYNumber) dy = double.NaN;
                 return dx.CompareTo(dy);
+            }
+        }
+
+        /// <summary>
+        /// ソート条件を表します。
+        /// </summary>
+        [Serializable]
+        public sealed class SortEntry : ICloneable, IHasArguments
+        {
+            /// <summary>
+            /// キーの列インデックスを取得または設定します。
+            /// </summary>
+            public int KeyIndex { get; set; }
+
+            /// <summary>
+            /// 数値として比較を行うかどうかを表す値を取得または設定します。
+            /// </summary>
+            public bool AsNumber { get; set; }
+
+            /// <summary>
+            /// 逆順ソートを行うかどうかを表す値を取得または設定します。
+            /// </summary>
+            public bool AsReversed { get; set; }
+
+            /// <summary>
+            /// 大文字小文字を区別するかどうかを表す値を取得または設定します。
+            /// </summary>
+            public bool CaseSensitive { get; set; } = true;
+
+            /// <summary>
+            /// 引数一覧を取得します。
+            /// </summary>
+            public IList<ArgumentInfo> Arguments => _arguments ??= GenerateArguments();
+
+            [NonSerialized]
+            private IList<ArgumentInfo>? _arguments;
+
+            /// <summary>
+            /// <see cref="SortEntry"/>の新しいインスタンスを初期化します。
+            /// </summary>
+            public SortEntry()
+            {
+            }
+
+            /// <summary>
+            /// <see cref="SortEntry"/>の新しいインスタンスを初期化します。
+            /// </summary>
+            /// <param name="cloned">複製するインスタンス</param>
+            /// <remarks>クローン用</remarks>
+            /// <exception cref="ArgumentNullException"><paramref name="cloned"/>が<see langword="null"/></exception>
+            private SortEntry(SortEntry cloned)
+            {
+                ArgumentNullException.ThrowIfNull(cloned);
+            }
+
+            /// <inheritdoc cref="ICloneable.Clone"/>
+            public SortEntry Clone() => new SortEntry(this);
+
+            object ICloneable.Clone() => Clone();
+
+            /// <summary>
+            /// 引数情報を生成します。
+            /// </summary>
+            /// <returns>引数情報一覧</returns>
+            private IList<ArgumentInfo> GenerateArguments()
+            {
+                return new[]
+                {
+                    new ArgumentInfo(ArgumentType.Index, "キーの列番号", () => KeyIndex, x => KeyIndex = x),
+                    new ArgumentInfo(ArgumentType.Boolean, "逆順ソート", () => AsReversed, x => AsReversed = x),
+                    new ArgumentInfo(ArgumentType.Boolean, "数値として比較", () => AsNumber, x => AsNumber = x),
+                    new ArgumentInfo(ArgumentType.Boolean, "大文字小文字の区別", () => CaseSensitive, x => CaseSensitive = x),
+                };
+            }
+
+            /// <inheritdoc/>
+            public ProcessStatus VerifyArguments()
+            {
+                return new ProcessStatus();
+            }
+
+            /// <summary>
+            /// 対応する<see cref="IComparer{T}"/>を取得します。
+            /// </summary>
+            /// <returns>対応する<see cref="IComparer{T}"/>のインスタンス</returns>
+            internal IComparer<List<string>> GetComparer()
+            {
+                IComparer<List<string>> result = AsNumber ? new IndexedNumberComparer(KeyIndex) : new IndexedStringComparer(!CaseSensitive, KeyIndex);
+                return AsReversed ? Comparer<List<string>>.Create((x, y) => -result.Compare(x, y)) : result;
             }
         }
     }
